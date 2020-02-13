06b26255c48476c43aafd391758e1a32773ac070
rito <ruitongl1@student.unimelb.edu.au>
2020-02-13 11:01:29 +0800

    Create 234.回文链表.py

---
+++ 234.回文链表.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=234 lang=python3
+#
+# [234] 回文链表
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+
