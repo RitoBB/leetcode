e5f839d786d88425de917a2813155359c7257231
rito <ruitongl1@student.unimelb.edu.au>
2020-02-14 17:06:14 +0800

    dynamic programming

---
+++ 62.不同路径.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=62 lang=python3
+#
+# [62] 不同路径
+#
+
+# @lc code=start
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        ## 初级版本
+        ## dp = [[1]*n] + [[1]+[0] * (n-1) for _ in range(m-1)]
+        dp = [[0]*n for _ in range(m)]
+        for i in range(m):
+            dp[i][0] = 1
+        for i in range(n):
+            dp[0][i] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[-1][-1]
+        ## 优化空间版本。 空间复杂度 O(2n)
+        pre = [1] * n
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] = pre[j] + cur[j-1]
+            pre = cur[:]
+        return pre[-1]
+        ## 优化空间版本。 空间复杂度 O(n)
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
+
+
+
+
+# @lc code=end
+
