6fa315a22af11e8a2fa246fdaad6928622d3ffc2
rito <ruitongl1@student.unimelb.edu.au>
2020-02-14 16:37:06 +0800

    dynamic programming

--- 53.最大子序和.py
+++ 53.最大子序和.py
@@ -14,11 +14,10 @@ class Solution:
         dp[0] = nums[0]
         ## step 3: 状态转移方程
         for i in range(1,len(nums)):
-            if dp[i-1]<0:
-                dp[i] = nums[i]
-            else:
-                dp[i] = nums[i]+dp[i-1]  
+             dp[i] = max(dp[i - 1] + nums[i], nums[i]) 
         return max(dp)
+
+        
 
 
         
