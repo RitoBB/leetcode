ff30c4c7ae3d46617c024c4cd77f6c26e9e470c1
rito <ruitongl1@student.unimelb.edu.au>
2020-02-14 16:34:15 +0800

    dynamic programming

---
+++ 53.最大子序和.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=53 lang=python3
+#
+# [53] 最大子序和
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ## step 1: 定义状态
+        ## dp[i]：表示以 nums[i] 结尾的连续子数组的最大和。
+        dp = [0]*len(nums)
+        ## step 2: 初始状态
+        dp[0] = nums[0]
+        ## step 3: 状态转移方程
+        for i in range(1,len(nums)):
+            if dp[i-1]<0:
+                dp[i] = nums[i]
+            else:
+                dp[i] = nums[i]+dp[i-1]  
+        return max(dp)
+
+
+        
+# @lc code=end
+
