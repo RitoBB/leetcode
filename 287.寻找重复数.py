0166211a6f1b6074ebe25876f8ba8281ec30264d
rito <ruitongl1@student.unimelb.edu.au>
2020-02-14 15:24:50 +0800

    array/two-pointers/binary-search

--- 287.寻找重复数.py
+++ 287.寻找重复数.py
@@ -8,7 +8,7 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         ##方法一：
-        ## return int((sum(nums) - sum(set(nums)))/ (len(nums) - len(set(nums))))
+        return int((sum(nums) - sum(set(nums)))/ (len(nums) - len(set(nums))))
         ## 方法三： 双指针法
         slow, fast = 0, 0
         while(1):
@@ -22,7 +22,6 @@ class Solution:
                     fast = nums[fast]
                     # print slow, fast
                 return nums[fast]
-        """
         ##方法二：二分法
         left, right = 1, len(nums) - 1
         while(left < right):
@@ -37,7 +36,7 @@ class Solution:
             else:
                 right = mid
         return left
-        """
+        
 
 # @lc code=end
 
