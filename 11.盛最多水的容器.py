3baceefde4662c2cdfba671fc0a9701913ca2918
rito <ruitongl1@student.unimelb.edu.au>
2020-03-30 11:31:58 +1100

    array

--- 11.盛最多水的容器.py
+++ 11.盛最多水的容器.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height)-1
+        m = 0
+        while left < right:
+            if height[left] < height[right]:
+                m = max(m, height[left]*(right-left))
+                left += 1
+            else:
+                m = max(m,height[right]*(right-left))
+                right -= 1
+        return m
+
         
 # @lc code=end
 
