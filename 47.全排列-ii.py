a6659692fa647d12aa6228586a044a25deb24e32
RitoBB <liu.3663@osu.edu>
2020-04-05 14:32:55 +1000

    backtracking

--- 47.全排列-ii.py
+++ 47.全排列-ii.py
@@ -8,6 +8,29 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
+        self.res = []
+        check = [0 for i in range(len(nums))]
+        
+        self.backtrack([], nums, check)
+        return self.res
+        
+    def backtrack(self, sol, nums, check):
+        if len(sol) == len(nums):
+            self.res.append(sol)
+            return
+        
+        for i in range(len(nums)):
+            if check[i] == 1:
+                continue
+            if i > 0 and nums[i] == nums[i-1] and check[i-1] == 0:
+                continue
+            check[i] = 1
+            self.backtrack(sol+[nums[i]], nums, check)
+            check[i] = 0
+
+"""
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         result = []
         def backtrack(nums , tmp):
             if not nums :
@@ -19,7 +42,7 @@ class Solution:
                     backtrack(nums[:i] + nums[i+1:] , tmp + [nums[i]])
         backtrack(nums , [])
         return result
-        
+"""        
         
         
 # @lc code=end
